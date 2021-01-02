64cf053b825be2b0ab5ce5d21ad2f8d9f8b2b12f
Simon Kruik <simon.karate.kid@gmail.com>
2021-01-02 21:10:02 +1100

    Ammending previous commit

--- vlc_discord_server.py
+++ vlc_discord_server.py
@@ -23,7 +23,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == 'list':
-        response = await
+        response = handle_task(message.guild.id, "LIST_LIBRARY")
         await message.channel.send(response)
 
     if message.content == 'join':
