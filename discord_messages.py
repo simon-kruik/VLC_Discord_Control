849ee20a3ab482f031e8906befa33ce07b668263
Simon Kruik <simon.karate.kid@gmail.com>
2021-01-08 08:39:36 +1100

    Amended calculation of total pages to be before video list shortening

--- discord_messages.py
+++ discord_messages.py
@@ -9,18 +9,19 @@ PREVIOUS_ARROW = "⬅"
 
 async def send_video_list(discord_client, video_list, channel_id, page=1):
     embed_message = discord.Embed(Title="Videos Available:", colour=COLOUR)
-    if (len(video_list) > PAGE_SIZE):
+    total_pages = ceil(len(video_list)/PAGE_SIZE)
+    if (total_pages > 1):
         start = (page-1) * PAGE_SIZE
         end = page * PAGE_SIZE
         video_list = video_list[start:end]
     for item in video_list:
         embed_message.add_field(name=item,value="\u200b", inline=False)
-    embed_message.set_footer(text="Page: " + str(page) + "/" + str(ceil(len(video_list)/PAGE_SIZE))) # e.g. Page 2/5
+    embed_message.set_footer(text="Page: " + str(page) + "/" + str(total_pages)) # e.g. Page 2/5
     channel = discord_client.get_channel(channel_id)
 
     message = await channel.send(embed=embed_message)
 
-    if len(video_list) > page * PAGE_SIZE:
+    if page < total_pages:
         await message.add_reaction(message, NEXT_ARROW)
     if page > 1:
         await message.add_reaction(message, PREVIOUS_ARROW)
